14aa5cfe36dbb2c8657fc5a17970244e4bd1a61a
frankgon1627 <fg1333762@gmail.com>
2025-07-16 17:48:20 -0400

    bug fixes

--- safety_controller/safety_controller.py
+++ safety_controller/safety_controller.py
@@ -1,29 +1,29 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
-from geometry_msgs.msg import Twist, TwistStamped
+from geometry_msgs.msg import TwistStamped
 
 class JoyTwistMux(Node):
     def __init__(self):
         super().__init__('joy_twist_mux')
         
         self.auto_cmd_sub = self.create_subscription(
-            Twist, '/j100_0000/autonomous/cmd_vel', self.auto_cmd_callback, 10)
+            TwistStamped, '/j100_0000/autonomous/cmd_vel', self.auto_cmd_callback, 10)
         self.joy_sub = self.create_subscription(
             Joy, '/j100_0000/joy_teleop/joy', self.joy_callback, 10)
         
         self.auto_mode_pub = self.create_publisher(TwistStamped, '/j100_0000/auto_mode/cmd_vel', 10)
 
-        self.auto_cmd = None
+        self.auto_cmd = TwistStamped()
 
-    def auto_cmd_callback(self, msg: Twist):
+    def auto_cmd_callback(self, msg: TwistStamped):
         self.auto_cmd = msg
 
     def joy_callback(self, msg: Joy):
         axis_value = msg.axes[4]
 
         if axis_value == 1.0:
-            self.cmd_pub.publish(self.auto_cmd)
+            self.auto_cmd.publish(self.auto_cmd)
 
 def main(args=None):
     rclpy.init(args=args)
